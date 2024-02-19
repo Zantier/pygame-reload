5cc926816a3e3bfdea21fa72ba56f6a7a2781831
David Phillips <zantier.tasa@gmail.com>
2024-02-19 16:05:34 +0000

    Position Window better on 1080p monitor

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pygame
 from pygame.locals import *
 import sys
 
-WINDOW_LEFT = 1500
-WINDOW_TOP = 100
-WINDOW_WIDTH = 1200
-WINDOW_HEIGHT = 1200
+WINDOW_LEFT = 800
+WINDOW_TOP = 50
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 600
 FPS = 30
 BACKGROUND_COLOR = (200, 250, 230)
 CIRCLE_COLOR = (230, 180, 180)
@@ -18,8 +18,8 @@ display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 
 def draw():
-    pygame.draw.circle(display_surface, CIRCLE_COLOR, (200, 200), 50)
-    pygame.draw.circle(display_surface, CIRCLE_COLOR, (400, 600), 130)
+    pygame.draw.circle(display_surface, CIRCLE_COLOR, (200, 100), 50)
+    pygame.draw.circle(display_surface, CIRCLE_COLOR, (300, 400), 130)
 
 
 while True:
